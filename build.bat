616e5100f1bf95f04b54d8b35ce7fd6a65a60e6a
anon <>
2025-11-21 14:02:42 +0800

    build: add build script

---
+++ build.bat
@@ -0,0 +1,2 @@
+gcc -o main.exe main.c  -I raylib/include -L raylib/lib -lraylib -lgdi32 -lwinmm
+gcc -o advanced_heart.exe advanced_heart.c  -I raylib/include -L raylib/lib -lraylib -lgdi32 -lwinmm
